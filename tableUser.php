ddee78881d468de0d14546d23c91ed1c55ca855d
forcela8 <57402030+forcela8@users.noreply.github.com>
2020-04-11 19:47:14 +0200

    Ajout des pages 404 et 500

--- tableUser.php
+++ tableUser.php
@@ -1,33 +1,10 @@
-<?php
-require_once "config.php";
-
-// For extra protection these are the columns of which the user can sort by (in your database table).
-$columns = array('name', 'email', 'rank', 'creationDate', 'active', 'ipAdress');
-
-// Only get the column if it exists in the above columns array, if it doesn't exist the database table will be sorted by the first item in the columns array.
-$column = isset($_GET['column']) && in_array($_GET['column'], $columns) ? $_GET['column'] : $columns[0];
-
-// Get the sort order for the column, ascending or descending, default is ascending.
-$sort_order = isset($_GET['order']) && strtolower($_GET['order']) == 'desc' ? 'DESC' : 'ASC';
-
-// Get the result...
-$sql = "SELECT * FROM `user` ORDER BY " . $column . ' ' . $sort_order;
-            $stmt = $con->prepare($sql);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            $nbRow = mysqli_num_rows($result);
-if($nbRow>0){
-	// Some variables we need for the table.
-	$up_or_down = str_replace(array('ASC','DESC'), array('up','down'), $sort_order); 
-	$asc_or_desc = $sort_order == 'ASC' ? 'desc' : 'asc';
-	$add_class = ' class="highlight"';
-	?>
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<title>PHP & MySQL Table Sorting by CodeShack</title>
-			<meta charset="utf-8">
-			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 			<style>
 			html {
 				font-family: Tahoma, Geneva, sans-serif;
@@ -69,67 +46,47 @@ if($nbRow>0){
 				background-color: #f9fafb;
 			}
 			</style>
-		</head>
-		<body>
-			<table>
-				<tr>
-					<th><a href="?column=name&order=<?php echo $asc_or_desc; ?>">Name<i class="fas fa-sort<?php echo $column == 'name' ? '-' . $up_or_down : ''; ?>"></i></a></th>
-					<th><a href="?column=age&order=<?php echo $asc_or_desc; ?>">Age<i class="fas fa-sort<?php echo $column == 'age' ? '-' . $up_or_down : ''; ?>"></i></a></th>
-					<th><a href="?column=joined&order=<?php echo $asc_or_desc; ?>">Join Date<i class="fas fa-sort<?php echo $column == 'joined' ? '-' . $up_or_down : ''; ?>"></i></a></th>
-				</tr>
-				<?php while ($row = $result->fetch_assoc()): ?>
-				<tr>
-					<td<?php echo $column == 'name' ? $add_class : ''; ?>><?php echo $row['name']; ?></td>
-					<td<?php echo $column == 'age' ? $add_class : ''; ?>><?php echo $row['age']; ?></td>
-					<td<?php echo $column == 'joined' ? $add_class : ''; ?>><?php echo $row['joined']; ?></td>
-				</tr>
-				<?php endwhile; ?>
-			</table>
-		</body>
-	</html>
-	<?php
-	$result->free();
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 <body>
     <table>
-        <tr>
-            <th><a href="?orderBy=name">Nom Prénom</a></th>
-            <th><a href="?orderBy=email">Email</a></th>
-            <th><a href="?orderBy=rank">Rang</a></th>
-            <th><a href="?orderBy=creationDate">Date de création</a></th>
-            <th><a href="?orderBy=active">Actif</a></th>
-            <th><a href="?orderBy=ipAdress">Adresse IP</a></th>
-        </tr>
-        <?php
+	<?php
         require_once "config.php";
         $orderBy = array('name', 'email', 'rank', 'creationDate', 'active', 'ipAdress');
         $order = 'name';
         if (isset($_GET['orderBy']) && in_array($_GET['orderBy'], $orderBy)) {
             $order = $_GET['orderBy'];
         }
-        
-            $sql = "SELECT * FROM `user` ORDER BY " .$order;
+        $sort_order = isset($_GET['order']) && strtolower($_GET['order']) == 'desc' ? 'DESC' : 'ASC';
+        if(!isset($_GET['orderBy']) || !isset($_GET['order'])){
+			$sql = "SELECT * FROM `user`";
+		}else{
+			$sql = "SELECT * FROM `user` ORDER BY " .$order . ' '. $sort_order;
+		}
             $stmt = $con->prepare($sql);
             $stmt->execute();
             $result = $stmt->get_result();
             $nbRow = mysqli_num_rows($result);
             if ($nbRow>0){
+				$up_or_down = str_replace(array('ASC','DESC'), array('up','down'), $sort_order); 
+				$asc_or_desc = $sort_order == 'ASC' ? 'desc' : 'asc';
+	?>
+        <tr>
+            <th><a href="?orderBy=name&order=<?php echo $asc_or_desc; ?>">Nom Prénom<i class="fas fa-sort<?php echo $column == 'name' ? '-' . $up_or_down : ''; ?>"></a></th>
+            <th><a href="?orderBy=email&order=<?php echo $asc_or_desc; ?>">Email<i class="fas fa-sort<?php echo $column == 'email' ? '-' . $up_or_down : ''; ?>"></a></th>
+            <th><a href="?orderBy=rank&order=<?php echo $asc_or_desc; ?>">Rang<i class="fas fa-sort<?php echo $column == 'rank' ? '-' . $up_or_down : ''; ?>"></a></th>
+            <th><a href="?orderBy=creationDate&order=<?php echo $asc_or_desc; ?>">Date de création<i class="fas fa-sort<?php echo $column == 'creationDate' ? '-' . $up_or_down : ''; ?>"></a></th>
+            <th><a href="?orderBy=active&order=<?php echo $asc_or_desc; ?>">Actif<i class="fas fa-sort<?php echo $column == 'active' ? '-' . $up_or_down : ''; ?>"></a></th>
+            <th><a href="?orderBy=ipAdress&order=<?php echo $asc_or_desc; ?>">Adresse IP<i class="fas fa-sort<?php echo $column == 'ipAdress' ? '-' . $up_or_down : ''; ?>"></a></th>
+        </tr>
+        <?php
                 while($row = $result->fetch_assoc()){
                     echo "<tr><td>". htmlspecialchars($row['name']). "</td><td>". htmlspecialchars($row['email']) . "</td><td>". htmlspecialchars($row['rank']) . "</td><td>". htmlspecialchars($row['creationDate']). "</td><td>". htmlspecialchars($row['active']). "</td><td>". htmlspecialchars($row['ipAdress']). "<button> ok</button></td>";
                 }
                 echo "</table>";
             }else{
                 echo "0 result";
-            }
+			}
+			
         ?>
     </table>
 </body>
